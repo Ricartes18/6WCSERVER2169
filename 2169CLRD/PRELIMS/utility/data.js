4e4344629a309780f98b0e389440d5292ee2be7e
Cedric Luis <63840601+Ricartes18@users.noreply.github.com>
2025-06-27 12:53:56 +0800

    Add files via upload

---
+++ 2169CLRD/PRELIMS/utility/data.js
@@ -0,0 +1,4 @@
+module.exports = {
+    firstname: 'Cedric',
+    lastname: 'Dungca'
+}
---
+++ 2169CLRD/app.js
@@ -0,0 +1,15 @@
+// var myLogModule = require('./PRELIMS/utility/log.js');
+
+// myLogModule.warning('Warning node not found.');
+// myLogModule.error('Error node encountered an error');
+// myLogModule.info('Node running...');
+
+// // var msg = require('./PRELIMS/utility/message.js');
+// // console.log(msg)
+
+// var person = require('./PRELIMS/utility/data.js');
+// console.log(person.firstname + ' ' + person.lastname);
+
+var msg = require('./PRELIMS/utility/log.js');
+
+msg('Hello World');
